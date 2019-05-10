68d76893b002db8bf6339ab17f358ed0e1fc5e70
tomhennigan <tomhennigan@google.com>
2019-05-10 09:34:13 +0100

    Add missing line continuation.
    PiperOrigin-RevId: 247572414

--- test.sh
+++ test.sh
@@ -19,6 +19,6 @@ pip install mock absl-py six numpy wrapt
 pip install --upgrade tf-nightly-2.0-preview
 
 # Run bazel test command. Double test timeouts to avoid flakes.
-bazel test --jobs=${N_JOBS} --test_timeout 300,450,1200,3600
+bazel test --jobs=${N_JOBS} --test_timeout 300,450,1200,3600 \
     --build_tests_only --test_output=errors -- \
     //sonnet/...

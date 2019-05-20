071ec1e1f4f6142b8a8ddce0b60c3fa564eda422
Sergei Lebedev <185856+superbobry@users.noreply.github.com>
2019-05-20 15:55:38 +0100

    Removed a new line in test.sh

--- test.sh
+++ test.sh
@@ -34,7 +34,6 @@ echo ""
 
 # Python test dependencies.
 python3 -m pip install -r requirements-test.txt
-
 python3 -m pip install --upgrade tf-nightly-2.0-preview==2.0.0.dev20190517
 python3 -c 'import tensorflow as tf; print(tf.__version__)'
 

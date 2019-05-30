7d43294fb7e833c791a3a4704055aee5973f63fb
Sergei Lebedev <slebedev@google.com>
2019-05-30 03:25:46 -0700

    Added an explicit master_doc
    Sphinx 2.0 defaults it to 'contents' whereas we use 'index'.
    
    PiperOrigin-RevId: 250659221
    Change-Id: I3adc694fb83da073247976cb319f58639e64cc02

--- docs/conf.py
+++ docs/conf.py
@@ -43,6 +43,8 @@ author = 'Sonnet Contributors'
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
@@ -63,7 +65,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 # -- Options for HTML output -------------------------------------------------
 

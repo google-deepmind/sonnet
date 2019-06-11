619559aeeeb9b16a3d2e6e520055782b480b70d5
Tom Hennigan <tomhennigan@google.com>
2019-06-11 13:35:16 -0700

    Add variable creation test between built-in Sonnet modules and TPUStrategy.
    PiperOrigin-RevId: 252687703
    Change-Id: I269b7dbeda7c7c9952eb09e23f4c4dca21d4123b

---
+++ sonnet/src/conformance/distribute_test.py
@@ -0,0 +1,59 @@
+# Copyright 2019 The Sonnet Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+"""Tests Sonnet and TF Distribution Strategy."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from absl.testing import parameterized
+from sonnet.src import test_utils
+from sonnet.src.conformance import goldens
+import tensorflow as tf
+
+
+class TPUStrategyTest(test_utils.TestCase, parameterized.TestCase):
+
+  @goldens.all_goldens
+  def test_variable_creation_in_replica_context(self, golden):
+    if self.primary_device != "TPU":
+      self.skipTest("Requires TPU")
+
+    strategy = tf.distribute.experimental.TPUStrategy()
+    with strategy.scope():
+      mod = golden.create_module()
+
+    @tf.function
+    def forward():
+      step = lambda: golden.create_all_variables(mod)
+      return strategy.experimental_run_v2(step)
+
+    # TODO(b/132329316) Remove when `xla.compile` allows tf.device(TPU).
+    with tf.device(None):
+      variables_per_replica = forward()
+
+    self.assertLen(variables_per_replica, golden.num_variables)
+
+    # Check all variables have the same value on each replica.
+    for per_replica in variables_per_replica:
+      per_replica = strategy.experimental_local_results(per_replica)
+      first_replica = per_replica[0]
+      for nth_replica in per_replica[1:]:
+        self.assertAllEqual(first_replica, nth_replica)
+
+if __name__ == "__main__":
+  # tf.enable_v2_behavior()
+  tf.test.main()

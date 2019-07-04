67f036929d6be76ef142767a5d088ea60211b8fa
Malcolm Reynolds <mareynolds@google.com>
2019-07-04 04:42:20 -0700

    Moved VQ-VAE to Sonnet 2.
    Implements model from https://arxiv.org/abs/1711.00937 - original TF 1 code written Aaron van den Oord.
    
    PiperOrigin-RevId: 256532556
    Change-Id: Icea20719b2dab8d1f85b3b1dfe4d5657a89ab9ba

---
+++ sonnet/src/nets/vqvae.py
@@ -0,0 +1,289 @@
+# Copyright 2018 The Sonnet Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Sonnet implementation of VQ-VAE https://arxiv.org/abs/1711.00937."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from sonnet.src import base
+from sonnet.src import initializers
+from sonnet.src import moving_averages
+
+import tensorflow as tf
+
+
+class VectorQuantizer(base.Module):
+  """Sonnet module representing the VQ-VAE layer.
+
+  Implements the algorithm presented in
+  'Neural Discrete Representation Learning' by van den Oord et al.
+  https://arxiv.org/abs/1711.00937
+
+  Input any tensor to be quantized. Last dimension will be used as space in
+  which to quantize. All other dimensions will be flattened and will be seen
+  as different examples to quantize.
+
+  The output tensor will have the same shape as the input.
+
+  For example a tensor with shape [16, 32, 32, 64] will be reshaped into
+  [16384, 64] and all 16384 vectors (each of 64 dimensions)  will be quantized
+  independently.
+
+  Attributes:
+    embedding_dim: integer representing the dimensionality of the tensors in the
+      quantized space. Inputs to the modules must be in this format as well.
+    num_embeddings: integer, the number of vectors in the quantized space.
+    commitment_cost: scalar which controls the weighting of the loss terms
+      (see equation 4 in the paper - this variable is Beta).
+  """
+
+  def __init__(self, embedding_dim, num_embeddings, commitment_cost,
+               dtype=tf.float32, name='vector_quantizer'):
+    """Initializes a VQ-VAE module.
+
+    Args:
+      embedding_dim: integer representing the dimensionality of the tensors in
+        the quantized space. Inputs to the modules must be in this format as
+        well.
+      num_embeddings: integer, the number of vectors in the quantized space.
+      commitment_cost: scalar which controls the weighting of the loss terms
+        (see equation 4 in the paper - this variable is Beta).
+      dtype: dtype for the embeddings variable, defaults to tf.float32.
+      name: name of the module.
+    """
+    super(VectorQuantizer, self).__init__(name=name)
+    self.embedding_dim = embedding_dim
+    self.num_embeddings = num_embeddings
+    self.commitment_cost = commitment_cost
+
+    embedding_shape = [embedding_dim, num_embeddings]
+    initializer = initializers.VarianceScaling(distribution='uniform')
+    self.embeddings = tf.Variable(initializer(embedding_shape, dtype),
+                                  name='embeddings')
+
+  def __call__(self, inputs, is_training):
+    """Connects the module to some inputs.
+
+    Args:
+      inputs: Tensor, final dimension must be equal to embedding_dim. All other
+        leading dimensions will be flattened and treated as a large batch.
+      is_training: boolean, whether this connection is to training data.
+
+    Returns:
+      dict containing the following keys and values:
+        quantize: Tensor containing the quantized version of the input.
+        loss: Tensor containing the loss to optimize.
+        perplexity: Tensor containing the perplexity of the encodings.
+        encodings: Tensor containing the discrete encodings, ie which element
+          of the quantized space each input element was mapped to.
+        encoding_indices: Tensor containing the discrete encoding indices, ie
+          which element of the quantized space each input element was mapped to.
+    """
+    flat_inputs = tf.reshape(inputs, [-1, self.embedding_dim])
+
+    distances = (tf.reduce_sum(flat_inputs ** 2, 1, keepdims=True)
+                 - 2 * tf.matmul(flat_inputs, self.embeddings)
+                 + tf.reduce_sum(self.embeddings ** 2, 0, keepdims=True))
+
+    encoding_indices = tf.argmax(- distances, 1)
+    encodings = tf.one_hot(encoding_indices, self.num_embeddings)
+
+    # NB: if your code crashes with a reshape error on the line below about a
+    # Tensor containing the wrong number of values, then the most likely cause
+    # is that the input passed in does not have a final dimension equal to
+    # self.embedding_dim. Ideally we would catch this with an Assert but that
+    # creates various other problems related to device placement / TPUs.
+    encoding_indices = tf.reshape(encoding_indices, tf.shape(inputs)[:-1])
+    quantized = self.quantize(encoding_indices)
+
+    e_latent_loss = tf.reduce_mean((tf.stop_gradient(quantized) - inputs) ** 2)
+    q_latent_loss = tf.reduce_mean((quantized - tf.stop_gradient(inputs)) ** 2)
+    loss = q_latent_loss + self.commitment_cost * e_latent_loss
+
+    # Straight Through Estimator
+    quantized = inputs + tf.stop_gradient(quantized - inputs)
+    avg_probs = tf.reduce_mean(encodings, 0)
+    perplexity = tf.exp(- tf.reduce_sum(
+        avg_probs * tf.math.log(avg_probs + 1e-10)))
+
+    return {
+        'quantize': quantized,
+        'loss': loss,
+        'perplexity': perplexity,
+        'encodings': encodings,
+        'encoding_indices': encoding_indices,
+        'distances': distances,
+    }
+
+  def quantize(self, encoding_indices):
+    """Returns embedding tensor for a batch of indices."""
+    w = tf.transpose(self.embeddings, [1, 0])
+    # TODO(mareynolds) in V1 we had a validate_indices kwarg, this is no longer
+    # supported in V2. Are we missing anything here?
+    return tf.nn.embedding_lookup(w, encoding_indices)
+
+
+class VectorQuantizerEMA(base.Module):
+  """Sonnet module representing the VQ-VAE layer.
+
+  Implements a slightly modified version of the algorithm presented in
+  'Neural Discrete Representation Learning' by van den Oord et al.
+  https://arxiv.org/abs/1711.00937
+
+  The difference between VectorQuantizerEMA and VectorQuantizer is that
+  this module uses exponential moving averages to update the embedding vectors
+  instead of an auxiliary loss. This has the advantage that the embedding
+  updates are independent of the choice of optimizer (SGD, RMSProp, Adam, K-Fac,
+  ...) used for the encoder, decoder and other parts of the architecture. For
+  most experiments the EMA version trains faster than the non-EMA version.
+
+  Input any tensor to be quantized. Last dimension will be used as space in
+  which to quantize. All other dimensions will be flattened and will be seen
+  as different examples to quantize.
+
+  The output tensor will have the same shape as the input.
+
+  For example a tensor with shape [16, 32, 32, 64] will be reshaped into
+  [16384, 64] and all 16384 vectors (each of 64 dimensions)  will be quantized
+  independently.
+
+  Attributes:
+    embedding_dim: integer representing the dimensionality of the tensors in the
+      quantized space. Inputs to the modules must be in this format as well.
+    num_embeddings: integer, the number of vectors in the quantized space.
+    commitment_cost: scalar which controls the weighting of the loss terms (see
+      equation 4 in the paper).
+    decay: float, decay for the moving averages.
+    epsilon: small float constant to avoid numerical instability.
+  """
+
+  def __init__(self, embedding_dim, num_embeddings, commitment_cost, decay,
+               epsilon=1e-5, dtype=tf.float32, name='vector_quantizer_ema'):
+    """Initializes a VQ-VAE EMA module.
+
+    Args:
+      embedding_dim: integer representing the dimensionality of the tensors in
+        the quantized space. Inputs to the modules must be in this format as
+        well.
+      num_embeddings: integer, the number of vectors in the quantized space.
+      commitment_cost: scalar which controls the weighting of the loss terms
+        (see equation 4 in the paper - this variable is Beta).
+      decay: float between 0 and 1, controls the speed of the Exponential
+        Moving Averages.
+      epsilon: small constant to aid numerical stability, default 1e-5.
+      dtype: dtype for the embeddings variable, defaults to tf.float32.
+      name: name of the module.
+    """
+    super(VectorQuantizerEMA, self).__init__(name=name)
+    self.embedding_dim = embedding_dim
+    self.num_embeddings = num_embeddings
+    if not 0 <= decay <= 1:
+      raise ValueError('decay must be in range [0, 1]')
+    self.decay = decay
+    self.commitment_cost = commitment_cost
+    self.epsilon = epsilon
+
+    embedding_shape = [embedding_dim, num_embeddings]
+    initializer = initializers.VarianceScaling(distribution='uniform')
+    self.embeddings = tf.Variable(
+        initializer(embedding_shape, dtype), name='embeddings')
+
+    self.ema_cluster_size = moving_averages.ExponentialMovingAverage(
+        decay=self.decay, name='ema_cluster_size')
+    self.ema_cluster_size.initialize(tf.zeros([num_embeddings], dtype=dtype))
+
+    self.ema_dw = moving_averages.ExponentialMovingAverage(
+        decay=self.decay, name='ema_dw')
+    self.ema_dw.initialize(self.embeddings)
+
+  def __call__(self, inputs, is_training):
+    """Connects the module to some inputs.
+
+    Args:
+      inputs: Tensor, final dimension must be equal to embedding_dim. All other
+        leading dimensions will be flattened and treated as a large batch.
+      is_training: boolean, whether this connection is to training data. When
+        this is set to False, the internal moving average statistics will not be
+        updated.
+
+    Returns:
+      dict containing the following keys and values:
+        quantize: Tensor containing the quantized version of the input.
+        loss: Tensor containing the loss to optimize.
+        perplexity: Tensor containing the perplexity of the encodings.
+        encodings: Tensor containing the discrete encodings, ie which element
+          of the quantized space each input element was mapped to.
+        encoding_indices: Tensor containing the discrete encoding indices, ie
+          which element of the quantized space each input element was mapped to.
+    """
+    flat_inputs = tf.reshape(inputs, [-1, self.embedding_dim])
+
+    distances = (tf.reduce_sum(flat_inputs ** 2, 1, keepdims=True)
+                 - 2 * tf.matmul(flat_inputs, self.embeddings)
+                 + tf.reduce_sum(self.embeddings ** 2, 0, keepdims=True))
+
+    encoding_indices = tf.argmax(- distances, 1)
+    encodings = tf.one_hot(encoding_indices, self.num_embeddings)
+
+    # NB: if your code crashes with a reshape error on the line below about a
+    # Tensor containing the wrong number of values, then the most likely cause
+    # is that the input passed in does not have a final dimension equal to
+    # self.embedding_dim. Ideally we would catch this with an Assert but that
+    # creates various other problems related to device placement / TPUs.
+    encoding_indices = tf.reshape(encoding_indices, tf.shape(inputs)[:-1])
+    quantized = self.quantize(encoding_indices)
+    e_latent_loss = tf.reduce_mean((tf.stop_gradient(quantized) - inputs) ** 2)
+
+    if is_training:
+      updated_ema_cluster_size = self.ema_cluster_size(
+          tf.reduce_sum(encodings, axis=0))
+
+      dw = tf.matmul(flat_inputs, encodings, transpose_a=True)
+      updated_ema_dw = self.ema_dw(dw)
+
+      n = tf.reduce_sum(updated_ema_cluster_size)
+      updated_ema_cluster_size = (
+          (updated_ema_cluster_size + self.epsilon)
+          / (n + self.num_embeddings * self.epsilon) * n)
+
+      normalised_updated_ema_w = (
+          updated_ema_dw / tf.reshape(updated_ema_cluster_size, [1, -1]))
+
+      self.embeddings.assign(normalised_updated_ema_w)
+      loss = self.commitment_cost * e_latent_loss
+
+    else:
+      loss = self.commitment_cost * e_latent_loss
+
+    # Straight Through Estimator
+    quantized = inputs + tf.stop_gradient(quantized - inputs)
+    avg_probs = tf.reduce_mean(encodings, 0)
+    perplexity = tf.exp(- tf.reduce_sum(
+        avg_probs * tf.math.log(avg_probs + 1e-10)))
+
+    return {
+        'quantize': quantized,
+        'loss': loss,
+        'perplexity': perplexity,
+        'encodings': encodings,
+        'encoding_indices': encoding_indices,
+        'distances': distances,
+    }
+
+  def quantize(self, encoding_indices):
+    """Returns embedding tensor for a batch of indices."""
+    w = tf.transpose(self.embeddings, [1, 0])
+    return tf.nn.embedding_lookup(w, encoding_indices)
---
+++ sonnet/src/nets/vqvae_test.py
@@ -0,0 +1,136 @@
+# Copyright 2018 The Sonnet Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Tests for sonnet.v2.src.nets.vqvae."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from absl.testing import parameterized
+
+import numpy as np
+from sonnet.src import test_utils
+from sonnet.src.nets import vqvae
+import tensorflow as tf
+
+
+class VqvaeTest(parameterized.TestCase, test_utils.TestCase):
+
+  @parameterized.parameters(
+      (vqvae.VectorQuantizer,
+       {'embedding_dim': 4, 'num_embeddings': 8,
+        'commitment_cost': 0.25}),
+      (vqvae.VectorQuantizerEMA,
+       {'embedding_dim': 6, 'num_embeddings': 13,
+        'commitment_cost': 0.5, 'decay': 0.1}))
+  def testConstruct(self, constructor, kwargs):
+    vqvae_module = constructor(**kwargs)
+    # Batch of input vectors to quantize
+    inputs_np = np.random.randn(100, kwargs['embedding_dim']).astype(np.float32)
+    inputs = tf.constant(inputs_np)
+
+    # Set is_training to False, otherwise for the EMA case just evaluating the
+    # forward pass will change the embeddings, meaning that some of our computed
+    # closest embeddings will be incorrect.
+    vq_output = vqvae_module(inputs, is_training=False)
+
+    # Output shape is correct
+    self.assertEqual(vq_output['quantize'].shape, inputs.shape)
+
+    vq_output_np = tf.nest.map_structure(lambda t: t.numpy(), vq_output)
+    embeddings_np = vqvae_module.embeddings.numpy()
+
+    self.assertEqual(embeddings_np.shape, (kwargs['embedding_dim'],
+                                           kwargs['num_embeddings']))
+
+    # Check that each input was assigned to the embedding it is closest to.
+    distances = ((inputs_np ** 2).sum(axis=1, keepdims=True)
+                 - 2 * np.dot(inputs_np, embeddings_np)
+                 + (embeddings_np**2).sum(axis=0, keepdims=True))
+    closest_index = np.argmax(-distances, axis=1)
+    # On TPU, distances can be different by ~1% due to precision. This can cause
+    # the distanc to the closest embedding to flip, leading to a difference
+    # in the encoding indices tensor. First we check that the continuous
+    # distances are reasonably close, and then we only allow N differences in
+    # the encodings. For batch of 100, N == 3 seems okay (passed 1000x tests).
+    self.assertAllClose(distances, vq_output_np['distances'], atol=4e-2)
+    num_differences_in_encodings = (
+        closest_index != vq_output_np['encoding_indices']).sum()
+    num_differences_allowed = 3
+    self.assertLessEqual(num_differences_in_encodings, num_differences_allowed)
+
+  @parameterized.parameters(
+      (vqvae.VectorQuantizer,
+       {'embedding_dim': 4, 'num_embeddings': 8,
+        'commitment_cost': 0.25}),
+      (vqvae.VectorQuantizerEMA,
+       {'embedding_dim': 6, 'num_embeddings': 13,
+        'commitment_cost': 0.5, 'decay': 0.1}))
+  def testShapeChecking(self, constructor, kwargs):
+    vqvae_module = constructor(**kwargs)
+    wrong_shape_input = np.random.randn(100, kwargs['embedding_dim'] * 2)
+    with self.assertRaisesRegexp(tf.errors.InvalidArgumentError,
+                                 'but the requested shape has'):
+      vqvae_module(tf.constant(wrong_shape_input.astype(np.float32)),
+                   is_training=False)
+
+  @parameterized.parameters(
+      (vqvae.VectorQuantizer,
+       {'embedding_dim': 4, 'num_embeddings': 8,
+        'commitment_cost': 0.25}),
+      (vqvae.VectorQuantizerEMA,
+       {'embedding_dim': 6, 'num_embeddings': 13,
+        'commitment_cost': 0.5, 'decay': 0.1})
+  )
+  def testNoneBatch(self, constructor, kwargs):
+    """Check that vqvae can be built on input with a None batch dimension."""
+    vqvae_module = constructor(**kwargs)
+    inputs = tf.zeros([0, 5, 5, kwargs['embedding_dim']])
+    vqvae_module(inputs, is_training=False)
+
+  @parameterized.parameters(
+      {'use_tf_function': True},
+      {'use_tf_function': False})
+  def testEmaUpdating(self, use_tf_function):
+    embedding_dim = 6
+    vqvae_module = vqvae.VectorQuantizerEMA(
+        embedding_dim=embedding_dim, num_embeddings=7,
+        commitment_cost=0.5, decay=0.1)
+    if use_tf_function:
+      vqvae_module = tf.function(vqvae_module)
+
+    batch_size = 16
+
+    prev_embeddings = vqvae_module.embeddings.numpy()
+
+    # Embeddings should change with every forwards pass if is_training == True.
+    for _ in range(10):
+      inputs = tf.random.normal([batch_size, embedding_dim])
+      vqvae_module(inputs, is_training=True)
+      current_embeddings = vqvae_module.embeddings.numpy()
+      self.assertFalse((prev_embeddings == current_embeddings).all())
+      prev_embeddings = current_embeddings
+
+    # Forward passes with is_training == False don't change anything
+    for _ in range(10):
+      inputs = tf.random.normal([batch_size, embedding_dim])
+      vqvae_module(inputs, is_training=False)
+      current_embeddings = vqvae_module.embeddings.numpy()
+      self.assertTrue((current_embeddings == prev_embeddings).all())
+
+
+if __name__ == '__main__':
+  # tf.enable_v2_behavior()
+  tf.test.main()

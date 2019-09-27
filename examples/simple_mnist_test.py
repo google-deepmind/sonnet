a4dd8093a92c73695e5e1f029dc93d54d3d4a966
Tamara Norman <tamaranorman@google.com>
2019-09-27 03:55:56 -0700

    Add test for simple_mnist example
    PiperOrigin-RevId: 271541189
    Change-Id: I6aff015b6c6ddf097b47a65fecd855ba59ac8a10

---
+++ examples/simple_mnist_test.py
@@ -0,0 +1,74 @@
+# Copyright 2019 The Sonnet Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Tests for sonnet.v2.examples.simple_mnist."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import sonnet as snt
+from sonnet.examples import simple_mnist
+from sonnet.src import test_utils
+import tensorflow as tf
+
+
+class SimpleMnistTest(test_utils.TestCase):
+
+  def setUp(self):
+    self.ENTER_PRIMARY_DEVICE = False  # pylint: disable=invalid-name
+    super(SimpleMnistTest, self).setUp()
+
+  def test_train_epoch(self):
+    model = snt.Sequential([
+        snt.Flatten(),
+        snt.Linear(10),
+    ])
+
+    optimizer = snt.optimizers.SGD(0.1)
+
+    dataset = tf.data.Dataset.from_tensor_slices(
+        (tf.random.normal([2, 8, 8, 1]),
+         tf.ones([2], dtype=tf.int64))).batch(2).repeat(4)
+
+    loss = simple_mnist.train_epoch(model, optimizer, dataset)
+    self.assertEqual(loss.shape, [])
+    self.assertEqual(loss.dtype, tf.float32)
+
+  def test_get_dataset(self):
+    dataset = simple_mnist.mnist("train", 4)
+
+    batch = next(iter(dataset))
+
+    self.assertEqual(batch[0].shape, [4, 28, 28, 1])
+    self.assertEqual(batch[1].shape, [4])
+    self.assertEqual(batch[0].dtype, tf.float32)
+    self.assertEqual(batch[1].dtype, tf.int64)
+
+  def test_test_accuracy(self):
+    model = snt.Sequential([
+        snt.Flatten(),
+        snt.Linear(10),
+    ])
+    dataset = tf.data.Dataset.from_tensor_slices(
+        (tf.random.normal([2, 8, 8, 1]),
+         tf.ones([2], dtype=tf.int64))).batch(2).repeat(4)
+
+    outputs = simple_mnist.test_accuracy(model, dataset)
+    self.assertEqual(len(outputs), 2)
+
+
+if __name__ == "__main__":
+  # tf.enable_v2_behavior()
+  tf.test.main()

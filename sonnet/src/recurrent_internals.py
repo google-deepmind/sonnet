6e3f454fa09a5510934caf7fd153de9cb2016f59
Sergii Volodko <svolodko@8thlight.com>
2020-06-28 23:16:09 +0100

    Add to recurrent_internals __future__ imports

--- sonnet/src/recurrent_internals.py
+++ sonnet/src/recurrent_internals.py
@@ -14,6 +14,10 @@
 # ============================================================================
 """Utils for Recurrent Neural Network cores."""
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import collections
 import functools
 import uuid
